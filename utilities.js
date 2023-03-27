fb84832324dc8b98eb623b99c65dd463f3e96867
Zahra Suleymanova <105732760+Zahra46031@users.noreply.github.com>
2023-03-27 19:22:42 -0400

    First Commit

---
+++ utilities.js
@@ -0,0 +1,23 @@
+
+
+function changeText(option) {
+    var button = document.getElementsByClassName("dropbtn")[0];
+    button.innerHTML = option;
+  }
+  
+  function changeText2(option) {
+    var button = document.getElementsByClassName("dropbtnend")[0];
+    button.innerHTML = option;
+  }
+  function changeText3(option) {
+    var button = document.getElementsByClassName("dropbtnArea")[0];
+    button.innerHTML = option;
+  }
+  function changeText4(option) {
+    var button = document.getElementsByClassName("dropbtnHotelChain")[0];
+    button.innerHTML = option;
+  }
+  function changeText5(option) {
+    var button = document.getElementsByClassName("dropbtnCapacity")[0];
+    button.innerHTML = option;
+  }
